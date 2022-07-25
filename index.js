071fa807eed702e38819d0e55aca4611c6b6a98c
NorthEastKnow <madhushudhanmondal1234@gmail.com>
2022-07-25 21:37:28 +0530

    submit

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const webpush = require("web-push");
 const express = require("express");
 const https = require('https');
 const http = require('http');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000 ;
-const server = http.createServer();
+//const server = http.createServer();
+const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
 
 const publicVapidKey = "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
 const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
@@ -14,47 +21,47 @@ webpush.setVapidDetails(
   "mailto:test@test.com",
   publicVapidKey,
   privateVapidKey);
+  
+
+  app.post("/",function(req,res){
+
+    const notifiTitle = req.body.titleNotification;
+    const notifiDescription = req.body.descriptionNotification;
+
+    https.get('https://pushnotificationapi.herokuapp.com/user', (resp) => {
+      let data = '';
+    
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        data += chunk;
+       
+      });
+    
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+       const obj = JSON.parse(data);
+       for(let i in obj.user)
+       {
+        const payload = JSON.stringify({title: 'Push Test'});
+    
+        //Pass object into sendNotification
+        webpush.sendNotification(obj.user[i].endpoint,payload).catch(err => console.error(err));
+       //console.log(obj.user[i].endpoint);
+       }
+      });
+    
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+    
+
+
+  });
 // fetch('https://pushnotificationapi.herokuapp.com/user')
 //   .then(response => response.json())
 //   .then(data => {
 //     console.log(data)
 //   })
 //   .catch(err => console.log(err));
-https.get('https://pushnotificationapi.herokuapp.com/user', (resp) => {
-  let data = '';
 
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-    //console.log(data[0].endpoint);
-     //data.forEach(element=> console.log(element));
-    // for(let i=0; i<data.length; i++)
-    // {
-    //     console.log(data[i]);
-    // }
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-   const obj = JSON.parse(data);
-   for(let i in obj.user)
-   {
-    const payload = JSON.stringify({title: 'Push Test'});
-
-    //Pass object into sendNotification
-    webpush.sendNotification(obj.user[i].endpoint,payload).catch(err => console.error(err));
-   //console.log(obj.user[i].endpoint);
-   }
-    //console.log(Object.keys(Object.keys(obj)[0])[1]);
-    
-   // console.log(jsonJSONstringfy(obj[0]));
-   // console.log(JSON.parse(data));
-    
-    //console.log(obj[endpoint]);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
-server.listen(port,console.log("app is running"));
+app.listen(port,console.log("app is running"));
